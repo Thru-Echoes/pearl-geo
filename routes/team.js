fc1e651be45a0d0faa27abdc0bcfa2be3ab9759b
Oliver Muellerklein <omuellerklein@berkeley.edu>
2016-06-21 17:40:06 -0700

    Working prototype with team page, 1 species page, and home page

--- routes/team.js
+++ routes/team.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var routerTeam = express.Router();
-var bodyParser = require('body-parser');
-var app = require('../app');
 
 routerTeam.get('/', function(req, res) {
     res.render('team', {
-        title: "Team Page"
+        title: "PEARL Team"
     });
 });
 
