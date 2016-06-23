7a179d961c78800a46411ea62c1b79c331a5584c
Oliver Muellerklein <omuellerklein@berkeley.edu>
2016-06-23 16:28:27 -0700

    Added how-to-img-raster R script and added AbCo species

---
+++ routes/abco.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var routerAbco = express.Router();
+var bodyParser = require('body-parser');
+var app = require('../app');
+
+routerAbco.get('/', function(req, res) {
+    res.render('abco', {
+        title: "Species: Abbreviata confusa"
+    });
+});
+
+// Grab data from document
+routerAbco.post('/', function(req, res) {
+
+    res.end();
+});
+
+module.exports = routerAbco;
